7ddf08e74eaf654bcd26b0e3530b33545790654c
rukayyatu aliyu dagachi <rukayadagaci2003@gmail.com>
2022-09-10 14:21:55 -0700

    puts file

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,13 +1,13 @@
 #include <stdio.h>
+
 /**
  * main - Entry point
- *
- * Description: prints "Programming is like building a multilingual puzzle"
  * 
  * Return: Always 0 (Success)
  */
+
 int main(void)
 {
-	puts("\"Programmming is like building a multilingual puzzle");
+	puts("\"Programmming is like bulding a multilingual puzzle");
 	return (0);
 }
