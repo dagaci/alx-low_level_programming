16da2cac16d2d1d1589f090baafb15f8ab18e830
rukayyatu aliyu dagachi <rukayadagaci2003@gmail.com>
2022-09-13 15:02:14 -0700

    0-putchar.c

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * main - program entry point
+ *
+ * Description: This is a program to print putchar
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+
+{
+	putchar('_');
+	putchar('p');
+	putchar('u');
+	putchar('t');
+	putchar('c');
+	putchar('h');
+	putchar('a');
+	putchar('r');
+	putchar('\n');
+
+	return (0);
+}
+
+ 
