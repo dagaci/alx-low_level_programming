443033be17a83ce68d4f10a55fc9355efb45b59c
rukayyatu aliyu dagachi <rukayadagaci2003@gmail.com>
2022-09-19 20:25:04 -0700

    0x05-pointers_arrays_strings

--- 0x05-pointers_arrays_strings/main.h
+++ 0x05-pointers_arrays_strings/main.h
@@ -1,5 +1,5 @@
-#ifndef MAIN.H
-#define MAIN.H 
+#ifndef MAIN_H
+#define MAIN_H 
 
 int _putchar(char c);
 void reset_to_98(int *n);
@@ -10,7 +10,7 @@ void print_rev(char *s);
 void rev_string(char *s);
 void puts2(char *str);
 void puts_half(char *str);
-void print_array(int *a, int *n);
+void print_array(int *a, int n);
 char *_strucpy(char *dest, char *src);
 int _atoi(char *s);
 
